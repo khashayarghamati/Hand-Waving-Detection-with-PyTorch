5966b47367e0864b94cbcba30e454a9a52c4b5a7
Khashayar <khashayarghamati@gmail.com>
2023-11-22 15:37:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Hand-Waving Detection with PyTorch
+
+## Overview
+
+This project involves training a Convolutional Neural Network (CNN) using PyTorch to detect hand-waving gestures in images. The dataset consists of images labeled with hand-waving and non-hand-waving classes. The trained model is evaluated on a separate test set to assess its performance.
+
+## Requirements
+
+- Python 3.x
+- PyTorch
+- torchvision
+- scikit-learn
+
+Install the required dependencies using the following command:
+
+```bash
+pip install torch torchvision scikit-learn
+```
+
+## Usage
+
+### 1. Prepare the Dataset
+
+
+### 2. Train the Model
+
+
+
+### 3. Evaluate the Model
+
+
+### 4. Customize
+
+Feel free to customize the code, model architecture, and hyperparameters based on your specific requirements.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+```
+
+Remember to replace placeholders like `handwave_cnn.pth` with the actual names of your files and directories. Additionally, include any specific instructions or details relevant to your project.
---
+++ main.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import torchvision.transforms as transforms
+import numpy as np
+
+
+import deeplake
+ds = deeplake.load('hub://activeloop/11k-hands')
+
+height, width = 800, 800
+# Define a simple CNN model
+class HandWaveCNN(nn.Module):
+    def __init__(self):
+        super(HandWaveCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(128 * (height // 8) * (width // 8), 128)
+        self.relu4 = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128, 2)  # Assuming binary classification (hand wave or not)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.relu3(self.conv3(x)))
+        x = self.flatten(x)
+        x = self.dropout(self.relu4(self.fc1(x)))
+        x = self.fc2(x)
+        return x
+
+# Instantiate the model
+model = HandWaveCNN()
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+tform = transforms.Compose([
+    transforms.ToPILImage(), # Must convert to PIL image for subsequent operations to run
+    transforms.Resize(800),
+    transforms.RandomRotation(20), # Image augmentation
+    transforms.ToTensor(), # Must convert to pytorch tensor for subsequent operations to run
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+])
+
+#PyTorch Dataloader
+dataloader= ds.pytorch(batch_size = 16, num_workers = 2,
+    transform = {'images': tform, 'labels': ['hand']}, shuffle=True)
+
+dataset_list = list(dataloader.dataset)
+
+# Split the list into training and testing sets
+train_size = int(0.8 * len(dataset_list))
+test_size = len(dataset_list) - train_size
+
+train_loader, test_loader = torch.utils.data.random_split(dataset_list, [train_size, test_size])
+
+
+# Training loop
+num_epochs = 1000
+
+for epoch in range(num_epochs):
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}')
+
+# Evaluate the model on the test set
+model.eval()
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        accuracy = torch.sum(predicted == labels).item() / len(labels)
+        print(f'Test accuracy: {accuracy}')
+
+# Save the trained model for later use
+torch.save(model.state_dict(), 'hand_waving_model.pth')
